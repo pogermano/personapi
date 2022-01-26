f2c286b404f55f3d5b6a14a5d1ac138e1237cc69
pogermano <pogermano@gmail.com>
2022-01-26 14:22:13 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+<h2>Desenvolvendo um sistema de gerenciamento de pessoas em API REST com Spring Boot</h2>
---
+++ src/main/java/com/w3g/personapi/entity/Person.java
@@ -0,0 +1,18 @@
+package com.w3g.personapi.entity;
+
+
+public class Person {
+
+     private Long id;
+
+
+    private String firstName;
+
+
+    private String lastName;
+
+
+    private String cpf;
+
+
+}
